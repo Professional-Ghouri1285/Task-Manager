122762bd06697f51653e02722834e8b2333c0d64
Professional-Ghouri1285 <professional.ghouri1285@gmail.com>
2026-09-14 22:40:02 +0500

    registered services

--- TaskManager.Api/Program.cs
+++ TaskManager.Api/Program.cs
@@ -7,6 +7,10 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddOpenApi();
+builder.Services.AddScoped<IProjectService, ProjectService>();
+builder.Services.AddScoped<ITaskService, TaskService>();
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<ICommentService, CommentService>();
 
 var app = builder.Build();
 
